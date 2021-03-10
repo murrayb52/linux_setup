42e19ff917a13163dfe02017878976c495d56e12
Murray Buchanan <murrayb52@gmail.com>
2021-03-10 16:02:23 +0200

    Polybar Option1 spotify.sh

---
+++ dotfiles/.config/polybar/Option1/spotify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Godkayaki ~
+# Show Spotify current song without mpd or any extra application other than spotify.
+
+if pgrep -x "spotify" > /dev/null; then
+    artist=`playerctl --player=spotify metadata artist`
+    title=`playerctl --player=spotify metadata title`
+    echo "" $artist "-" $title
+else
+    echo ""
+fi
