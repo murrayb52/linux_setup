ee1ad6ca57b411176569c94b49d1e96701d552d9
Murray Buchanan <murrayb52@gmail.com>
2021-03-11 23:26:24 +0200

    Created spotify.sh for polybar Option4-Green

---
+++ dotfiles/.config/polybar/Option4-Green/spotify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+player_status=$(playerctl status 2> /dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+	if [[ $(playerctl metadata --format "{{artist}}" 2> /dev/null) ]]; then
+		echo "$(playerctl metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
+	else
+		echo "$(playerctl metadata --format "{{title}}" 2> /dev/null)"
+	fi
+elif [ "$player_status" = "Paused" ]; then
+    echo "Paused"
+else
+    echo "Failed to fetch Spotify data"
+fi
