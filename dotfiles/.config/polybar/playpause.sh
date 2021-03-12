f17d58cbbfbf333f4af092c128df9d5031fcb396
Murray Buchanan <murrayb52@gmail.com>
2021-03-12 11:31:15 +0200

    Removed old polybar files

--- dotfiles/.config/polybar/playpause.sh
+++
@@ -1,9 +0,0 @@
-#Taken from: https://github.com/reedrw/dotfiles/blob/master/config/polybar/playpause.sh
-
-#!/bin/sh
-
-if ! mpc status | grep -q "playing"; then
-    echo ""
-else
-    echo ""
-fi
