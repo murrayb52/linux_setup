3aeff43325902fef5f55ac159292b555a1881296
Murray Buchanan <murrayb52@gmail.com>
2021-03-12 10:42:05 +0200

    playpause.sh for polybar Option4-Green

---
+++ dotfiles/.config/polybar/Option4-Green/playpause.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Author: Murray Buchanan
+
+player_status=$(playerctl status 2> /dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+    echo ""
+elif [ "$player_status" = "Paused" ]; then
+    echo ""
+else
+    echo ""
+fi
