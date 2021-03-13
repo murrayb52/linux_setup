1a1d923ddf4004dee61b8000d9d7b3ba52fb4a1e
Murray Buchanan <murrayb52@gmail.com>
2021-03-13 11:27:47 +0200

    Apply media data fetching only to spotify

--- dotfiles/.config/polybar/Option4-Green/spotify.sh
+++ dotfiles/.config/polybar/Option4-Green/spotify.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-player_status=$(playerctl status 2> /dev/null)
+player_status=$(playerctl -p spotify status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-	if [[ $(playerctl metadata --format "{{artist}}" 2> /dev/null) ]]; then
-		echo "$(playerctl metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
+	if [[ $(playerctl -p spotify metadata --format "{{artist}}" 2> /dev/null) ]]; then
+		echo "$(playerctl -p spotify metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
 	else
-		echo "$(playerctl metadata --format "{{title}}" 2> /dev/null)"
+		echo "$(playerctl -p spotify metadata --format "{{title}}" 2> /dev/null)"
 	fi
 elif [ "$player_status" = "Paused" ]; then
     echo "Paused"
